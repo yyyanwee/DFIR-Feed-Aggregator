2556d327f6ff5efb719da7757ccd38f4b702b4c2
yyyanwee <89122919+yyyanwee@users.noreply.github.com>
2025-02-07 17:08:19 +0800

    add basic functions

--- src/feed_aggregator.py
+++ src/feed_aggregator.py
@@ -1,18 +1,18 @@
 import os
+import sys
 import time
 import feedparser
 import yaml
-from pycti import OpenCTIConnectorHelper, get_config_variable, Stix
-
+import stix2
+from pycti import OpenCTIConnectorHelper, get_config_variable
 
 
 # OpenCTI Connector Initialization
 config = {
     "opencti_url": os.getenv("OPENCTI_URL"),
     "opencti_token": os.getenv("OPENCTI_TOKEN"),
-    "rss_feed_urls": "https://example.com/rss",
-    "rss_feed_keywords": []
-
+    "rss_feed_urls": os.getenv("FEED_URLS"), #list of RSS feed URLs
+    "rss_feed_keywords": os.getenv("FEED_KEYWORDS"), #list of keywords to filter
 }
 
 class FeedAggregator:
@@ -27,14 +27,25 @@ class FeedAggregator:
         self.rss_feed_urls = config["rss_feed_urls"]
         self.rss_feed_keywords = config["rss_feed_keywords"]
 
-    def
+    def filter_feed(self, feed):
+        for keyword in self.rss_feed_keywords:
+            if keyword in feed.title:
+                return True
+        return False
+    
+    def run(self):
+        for feed_url in self.rss_feed_urls:
+            feed = feedparser.parse(feed_url)
+            for entry in feed.entries:
+                if self.filter_feed(entry):
+                    self.create_stix2_indicator(entry)
 
 
     
 if __name__ == "__main__":
     try:
         Feed_Aggregator = FeedAggregator()
-        cyberMonitorConnector.run()
+        Feed_Aggregator.run()
     except Exception as e:
         print(e)
         time.sleep(10)
