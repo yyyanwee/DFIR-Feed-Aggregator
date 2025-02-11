558ba14fc094b66f9369fad0304517fa243cfb90
yyyanwee <yanwee.loo.2020@scis.smu.edu.sg>
2025-02-12 00:08:49 +0800

    rewrite methods

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Feed Aggregator Connector
 
-The job of this connector is to poll multiple RSS Feeds at a regular schedule, parsing the feeds using `feedparser. The connector accomplishes a few key tasks:
+The job of this connector is to poll multiple RSS Feeds at a regular schedule, parsing the feeds using `feedparser`. The connector accomplishes a few key tasks:
 1. Periodically poll multiple RSS feeds
-2. Detect and process only new entries
-3. Maintain a mechanism to track already processed entries
-4. Convert feed entries into [STIX2 Report format](https://docs.oasis-open.org/cti/stix/v2.1/os/stix-v2.1-os.html#_n8bjzg1ysgdq)
-5. Push new reports to OpenCTI Platform
+2. Detect and process new entries
+3. Convert feed entries into [STIX2 Report format](https://docs.oasis-open.org/cti/stix/v2.1/os/stix-v2.1-os.html#_n8bjzg1ysgdq)
+4. Push new reports to OpenCTI Platform
 
 
 The connector currently covers the following RSS Feeds:
@@ -14,15 +13,7 @@ The connector currently covers the following RSS Feeds:
 | The Hacker News | https://feeds.feedburner.com/TheHackersNews |
 | Bleeping Computer | https://www.bleepingcomputer.com/feed/|
 |  | |
-|  | |
 
-The following rules are applied to the feeds:
-| Rule |    Description |
-| ----------- | ----------- |
-| Category Filter |  |
-| Ad Removal | |
-|  | |
-|  | |
 
 ## Installation
 The Feed Aggregator connector is a standalone Python process that must have access
@@ -47,16 +38,56 @@ Below are the parameters you'll need to set to run the connector properly:
 | ID                    | `id`                | `CONNECTOR_ID`                | /            | Yes       | A unique `UUIDv4` identifier for this connector instance.                                        |
 | Name                  | `name`              | `CONNECTOR_NAME`              | `FeedAggregator` | Yes       | Full name of the connector : `Feed_Aggregator`.                                                       |
 
+## How it works
 
-#### Reference Materials
-PyCTI: https://opencti-client-for-python.readthedocs.io/en/stable/
+FeedParser processes RSS Feeds and extract entries. 
+The entry is filtered according to certain rules to remove unwanted information. 
 
-FeedParser: https://feedparser.readthedocs.io/en/latest/
+The following rules are applied to the feeds:
+| Rule |    Description | Feeds Applied |
+| ----------- | ----------- | ----------- |
+| Category Filter | Selects articles of certain categories | Bleeping Computer |
+| Ad Removal | Removes unnecessary ad posts or summary posts | All |
+|  | |
 
-OpenCTI: https://docs.opencti.io/latest/deployment/connectors/#introduction
+
+The filtered entry is then converted into Stix2 Report Format and sent to OpenCTI Platform. 
+
+#### Example Report Format
+```
+{
+  "type": "report",
+  "spec_version": "2.1",
+  "id": "report--84e4d88f-44ea-4bcd-bbf3-b2c1c320bcb3",
+  "created": "2015-12-21T19:59:11.000Z",
+  "modified": "2015-12-21T19:59:11.000Z",
+  "created_by_ref": "identity--a463ffb3-1bd9-4d94-b02d-74e4f1658283",
+  "name": "The Black Vine Cyberespionage Group",
+  "description": "A simple report with an indicator and campaign",
+  "published": "2016-01-20T17:00:00.000Z",
+  "report_types": ["campaign"],
+  "object_refs": [
+    "indicator--26ffb872-1dd9-446e-b6f5-d58527e5b5d2",
+    "campaign--83422c77-904c-4dc1-aff5-5c38f3a2c55c",
+    "relationship--f82356ae-fe6c-437c-9c24-6b64314ae68a"
+  ]
+}
+## type, spec_version, created, modified, name, published, object_refs are required ###
+```
+## Reference Materials
+
+<b>PyCTI:</b>  
+https://opencti-client-for-python.readthedocs.io/en/stable/
+
+<b>FeedParser:</b>  
+https://feedparser.readthedocs.io/en/latest/
+
+<b>OpenCTI:</b>  
+https://docs.opencti.io/latest/deployment/connectors/#introduction
 https://docs.opencti.io/latest/development/connectors/
 
-Stix: https://oasis-open.github.io/cti-documentation/stix/intro.html
+<b>Stix2:</b>  
+https://oasis-open.github.io/cti-documentation/stix/intro.html
     
 
 
--- src/feed_aggregator.py
+++ src/feed_aggregator.py
@@ -1,22 +1,13 @@
 import datetime
 import os
+import re
 import sys
 import time
 import uuid
 import feedparser
 import yaml
 import stix2
-from pycti import OpenCTIConnectorHelper, get_config_variable
-
-
-# OpenCTI Connector Initialization
-# config = {
-#     "opencti_url": os.getenv("OPENCTI_URL"),
-#     "opencti_token": os.getenv("OPENCTI_TOKEN"),
-#     "rss_feed_urls": os.getenv("FEED_URLS"), #list of RSS feed URLs
-#     "keyword_filters": os.getenv("FEED_KEYWORDS"), #list of keywords to filter
-#
-# }
+from pycti import OpenCTIConnectorHelper, get_config_variable, Report
 
 class FeedAggregator:
     def __init__(self):
@@ -26,6 +17,7 @@ class FeedAggregator:
         #     if os.path.isfile(config_file_path)
         #     else {}
         # )
+
         #hardcode testing version
         self.config ={
             "OPENCTI_URL": "http://opencti:8080",
@@ -34,102 +26,100 @@ class FeedAggregator:
             "keyword_filters": ["security"],
             "feed_keyword_blacklist": ["top"] #not exhaustive
         }
+
         #self.helper = OpenCTIConnectorHelper(self.config) 
         self.rss_feed_urls = self.config["rss_feed_urls"]
         self.keyword_filters = self.config["keyword_filters"]
         self.feed_keyword_blacklist = self.config["feed_keyword_blacklist"]
         
-
+        # to track already processed entries
         self.processed_entries = set()
 
     def _load_state(self):
         #Loads previously processed entry identifiers from connector state
         state = self.helper.get_state()
-        if state and 'processed_entries' in state:
-            self.processed_entries = set(state['processed_entries'])
+        if state and "processed_entries" in state:
+            self.processed_entries = set(state["processed_entries"])
 
     def _save_state(self):
         #Saves current processed entries to connector state.
         self.helper.set_state({
-            'processed_entries': list(self.processed_entries)
+            "processed_entries": list(self.processed_entries)
         })
-
-    def filter_feeds(self, feed):
-        # returns a list of feed entries which contains the keywords in its "category" field
-        # currently affects the following feeds: Bleeping Computer
-        try:
-            filtered_items = [item for item in feed.entries
-            if any(keyword.lower() in (item.category).lower() for keyword in self.keyword_filters)]
-        except AttributeError:
-            filtered_items = [item for item in feed.entries]
-
-        # remove items with blacklisted keywords - remove ad posts or summary posts
-        try:
-            filtered_items = [item for item in filtered_items
-            if not any(keyword.lower() in (item.title).lower() for keyword in self.feed_keyword_blacklist)]
-        except AttributeError as e:
-            print(e)
-            pass
-
-        return filtered_items
     
+    def _is_new_entry(self,entry) -> bool:
+        # Checks if RSS entry is new and should be processed
+        return
+
     #Helper function
     def _create_stix_report(self, entry: dict[str, any]) -> stix2.Report:
         #Convert RSS entry into a STIX2 Report object.
-        return stix2.Report(
-            type="report",
-            name=entry.get('title', datetime.datetime.today().now().isoformat()),
-            id=f"report--{str(uuid.uuid4())}",
-            description=entry.get('summary', ''),
-            published=datetime.date.today(),
-            object_refs=["indicator--26ffb872-1dd9-446e-b6f5-d58527e5b5d2"],  # You can add more refs if needed
+        report_name = entry.title
+        report_date = entry.published
 
-            #optional below here
-            labels=['rss-feed', entry.get('source', 'unknown')],          
-            created_by_ref=f"identity--{self.config['OPENCTI_TOKEN']}"
-        )
-    
+        report = stix2.Report(
+                        id=Report.generate_id(report_name, report_date),
+                        name=report_name,
+                        published=report_date, # TODO: Change report date to supported datetime object
+                        external_references=[entry.link],
+                        object_refs=[] #object_refs=[self.dummy_organization["standard_id"]] 
+                    )
+        return report
+
+
+    def filter_entry(self, entry):
+        is_okay = True
+
+        # Category Filter
+        # Returns the entry if it contains the keywords in its "category" field
+        # Applies to the following feeds: Bleeping Computer
+        try:
+            if any(keyword.lower() in (entry['tags'][0]['term']).lower() for keyword in self.keyword_filters):
+                filtered = entry # TODO: Condition too specific, feeds that have different format will crash this 
+        except AttributeError: # TODO: Change Error Type
+            filtered = entry
+
+        # Title-based exclusion using regex
+        # Attempts to remove unnecessary entries with titles such as "Top xx in 2025" or ad posts
+        # List is not exhaustive, but we try our best!
+        exclusions = [
+            r'Top \d+ .*', 
+            r'Best \d+ .*',  # Best xxx"
+            r'.*Sponsored.*',  # Sponsored by ...
+            r'Advertisement.*'  # Advertisements Posts
+        ]
+        for pattern in exclusions:
+            regex = re.compile(pattern, re.IGNORECASE)
+            is_okay = not regex.match(filtered.title)
+
+        return is_okay
+        
+
     def process_rss_feeds(self):
         #self._load_state()
 
-        for feed_url in self.config['rss_feeds']:
-            
+        for url in self.rss_feed_urls:
+            feed = feedparser.parse(url)
             for entry in feed.entries:
-                if self._is_entry_new(entry):
+                if True: #self._is_entry_new(entry):
                     try:
-                        report = self._create_stix_report(entry)
-                        # Create report in OpenCTI
-                        #self.helper.stix2_create_report(report)
-                        
-                        # Track processed entry
-                        self.processed_entries.add(entry.get('id', ''))
+                        if(self.filter_entry(entry)):
+                            report = self._create_stix_report(entry)
+
+                            # Track processed entry
+                            self.processed_entries.add(report.get('id'))
                         
                     except Exception as e:
-                        self.helper.log_error(f"Error processing entry: {e}")
+                        #self.helper.log_error(f"Error processing entry: {e}")
+                        print(e)
 
         # Save after processing
         #self._save_state()
     
-    def send_to_opencti(self):
-        bundle_objects = []
-        bundle_objects.append(self.processed_entries) # should be the new entries only
 
-        bundle = stix2.Bundle(objects=bundle_objects).serialize()
-        print(bundle)
-        #bundles_sent = self.opencti_connector_helper.send_stix2_bundle(bundle) #assign variable for logging purposes
-    
     def run(self):
-        for feed_url in self.rss_feed_urls:
-            print(feed_url)
-            feed = feedparser.parse(feed_url)
-            filtered_entries = self.filter_feeds(feed)
-            for entry in filtered_entries:
-                print(entry.title)
-                print(entry.description)
-                print("")
-                report = self._create_stix_report(entry)
-                print(report)
-            print("next site")
+        self.process_rss_feeds()
+        print(self.processed_entries)
 
     def process(self):
         self.run()
