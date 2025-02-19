b9e547f68c2b538087aea4de9460fc3c33916716
yyyanwee <89122919+yyyanwee@users.noreply.github.com>
2025-02-19 15:48:54 +0800

    docker testing

--- src/feed_aggregator.py
+++ src/feed_aggregator.py
@@ -3,10 +3,12 @@ import os
 import re
 import sys
 import time
+import yaml
 import feedparser
 import dateparser
 import stix2
 from pycti import OpenCTIConnectorHelper, Report
+
 
 """
 TODO:
@@ -16,34 +18,36 @@ TODO:
 
 class FeedAggregator:
     def __init__(self):
-        #config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
-        # config = (
-        #     yaml.load(open(config_file_path), Loader=yaml.SafeLoader)
-        #     if os.path.isfile(config_file_path)
-        #     else {}
-        # )
+        config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
+        config = (
+            yaml.load(open(config_file_path), Loader=yaml.SafeLoader)
+            if os.path.isfile(config_file_path)
+            else {}
+        )
 
         #hardcode testing version
+        '''
         self.config ={
             "OPENCTI_URL": "http://opencti:8080",
             "OPENCTI_TOKEN": "d4f4d2b0-6b3b-4f6b-8d2d-3f8f6c6d8b4d",
             "feed_urls": [("Bleeping Computer","https://www.bleepingcomputer.com/feed/"), ("The Hackers News","https://feeds.feedburner.com/TheHackersNews")],
             "keyword_filters": ["security"],
         }
+        '''
 
-        #self.helper = OpenCTIConnectorHelper(self.config) 
-        self.rss_feed_urls = self.config["feed_urls"]
-        self.keyword_filters = self.config["keyword_filters"]
+        self.helper = OpenCTIConnectorHelper(config) 
+        self.rss_feed_urls = config["feed_urls"]
+        self.keyword_filters = config["keyword_filters"]
         
         # to track previously processed entries
         self.processed_entries = set()
         self.rejected_entries = set()
 
-        # self.dummy_organization = self.helper.api.identity.create(
-        #     type="Organization",
-        #     name="DUMMY",
-        #     description="Dummy organization which can be used in various unknown contexts.",
-        # )
+        self.dummy_organization = self.helper.api.identity.create(
+            type="Organization",
+            name="DUMMY",
+            description="Dummy organization which can be used in various unknown contexts.",
+        )
 
     def _get_state(self):
         #Loads previously processed entry identifiers from connector state
@@ -62,7 +66,7 @@ class FeedAggregator:
         #Convert RSS entry into a STIX2 Report object.
         report_name = entry.title
         report_date = dateparser.parse(entry.published)
-        external_reference = stix2.ExternalReference(source_name=source, url=entry.link) # TODO: Find a way to name external reference according to source name
+        external_reference = stix2.ExternalReference(source_name=source, url=entry.link)
 
         report = stix2.Report(
                         id=Report.generate_id(report_name, report_date),
@@ -70,8 +74,8 @@ class FeedAggregator:
                         published=report_date,
                         description = entry["summary"],
                         external_references=[external_reference],
-                        object_refs=["indicator--26ffb872-1dd9-446e-b6f5-d58527e5b5d2"] # TODO: Figure this out, cannot be empty
-                        #object_refs=[self.dummy_organization["standard_id"]] 
+                        # TODO: Figure this out, cannot be empty
+                        object_refs=[self.dummy_organization["standard_id"]] 
                     )
         print(report)
         print("")
@@ -163,7 +167,7 @@ class FeedAggregator:
 
     def process_rss_feeds(self):
         # Get the set of already processed entries
-        #self._get_state()
+        self._get_state()
 
         for detail in self.rss_feed_urls:
             feed = feedparser.parse(detail[1])
@@ -176,40 +180,39 @@ class FeedAggregator:
 
                         # send report to OpenCTI
                         bundle = stix2.Bundle(objects=report).serialize()
-                        #self.helper.send_stix2_bundle(bundle, entities_types=self.helper.connect_scope)
+                        self.helper.send_stix2_bundle(bundle, entities_types=self.helper.connect_scope)
 
                         # Save after sending
-                        # self._save_state()
+                        self._save_state()
                     else:
                         self.rejected_entries.add(entry)
                 except Exception as e:
-                        #self.helper.log_error(f"Error processing entry: {e}")
+                        self.helper.log_error(f"Error processing entry: {e}")
                         print(e)
 
         # Final save after processing
-        #self._save_state()
+        self._save_state()
 
 
     def run(self):
         print(f"Running Feed Aggregator - Run")
-        NumBefore = len(self.processed_entries)
+        # NumBefore = len(self.processed_entries)
         self.process_rss_feeds()
 
-        NumReportsAdded = len(self.processed_entries) - NumBefore
-        print(f"Number of reports added: {NumReportsAdded}")
+        # NumReportsAdded = len(self.processed_entries) - NumBefore
+        # print(f"Number of reports added: {NumReportsAdded}")
 
-        #For ease of debugging/tracking
-        rejected_entries_dict = {"rejected_entries": list(self.rejected_entries)}
-        json.dump(rejected_entries_dict["rejected_entries"], open("processed_entries.txt", "w"))
+        # #For ease of debugging/tracking
+        # rejected_entries_dict = {"rejected_entries": list(self.rejected_entries)}
+        # json.dump(rejected_entries_dict["rejected_entries"], open("processed_entries.txt", "w"))
 
-        print("Rejected Entries\n")
-        print(f"Number of rejected entries: {len(self.rejected_entries)}")
-
-        print(f"Running Feed Aggregator - End")
+        # print("Rejected Entries\n")
+        # print(f"Number of rejected entries: {len(self.rejected_entries)}")
+        # print(f"Running Feed Aggregator - End")
 
     def process(self):
         self.run()
-        time.sleep(15) #Set to 12 hours before going live (43200)
+        time.sleep(120) #Set to 12 hours before going live (43200)
     
 if __name__ == "__main__":
     try:
